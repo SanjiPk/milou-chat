8d5e0fe9526e1cc7e458422b48cfdffb15e6ca80
sanji <zero.0102003@gmail.com>
2025-06-12 13:00:09 +0330

    test home page and login page.

--- src/main/java/aut/ap/Main.java
+++ src/main/java/aut/ap/Main.java
@@ -1,9 +1,14 @@
 package aut.ap;
 
 import aut.ap.UI.LoginForm;
+import aut.ap.controller.EmailController;
 import aut.ap.controller.UserController;
+import aut.ap.entity.User;
+import aut.ap.repository.EmailRepository;
 import aut.ap.repository.UserRepository;
+import aut.ap.service.EmailService;
 import aut.ap.service.UserService;
+import aut.ap.validation.EmailValidator;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +16,11 @@ public class Main {
         UserService userService = new UserService(userRepository);
         UserController userController = new UserController(userService);
 
-        LoginForm loginForm = new LoginForm(userController);
+        EmailRepository emailRepository = new EmailRepository();
+        EmailService emailService = new EmailService(emailRepository);
+        EmailValidator emailValidator = new EmailValidator();
+        EmailController emailController = new EmailController(emailService, userController, emailValidator);
+
+        LoginForm loginForm = new LoginForm(userController, emailController);
     }
 }
